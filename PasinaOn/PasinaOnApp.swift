55e16b9f35376161d13fe08572c1f8a6588d3e70
Rizki Fitrani <fitriarizki2805@gmail.com>
2026-06-11 21:25:14 +0700

    Initial Commit

---
+++ PasinaOn/PasinaOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasinaOnApp.swift
+//  PasinaOn
+//
+//  Created by Rizki Fitriani on 11/06/26.
+//
+
+import SwiftUI
+
+@main
+struct PasinaOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
